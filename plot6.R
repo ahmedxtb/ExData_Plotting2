ba88bcf51769407f15453321284091b2072e95e2
ppaquay <p.paquay@linuxaddict.be>
2014-05-18 15:44:05 +0200

    Modif of README

--- plot6.R
+++ plot6.R
@@ -20,7 +20,7 @@ library(ggplot2)
 total.emissions.baltimore.LA.motor.vehicle <- aggregate(NEI.baltimore.LA.motor.vehicle$Emissions, list(year = NEI.baltimore.LA.motor.vehicle$year, region = NEI.baltimore.LA.motor.vehicle$fips), sum, na.rm = TRUE)
 colnames(total.emissions.baltimore.LA.motor.vehicle)[3] <- "emissions"
 
-## Compute the delta between each year and 1999 to easier visualise the
+## Compute the delta between each year and 1999 to better visualise the
 ## changes in emissions from motor vehicle sources for Baltimore City
 ## and Los Angeles County
 
@@ -33,5 +33,5 @@ total.emissions.baltimore.LA.motor.vehicle$region[total.emissions.baltimore.LA.m
 ## for Baltimore City and Los Angeles County (ggplot2 plotting system)
 
 png(file ="plot6.png", width = 480, height = 480)
-qplot(year, emissions, data = total.emissions.baltimore.LA.motor.vehicle, color = region, shape = region, geom = c("point", "line")) + ggtitle("Changes in emissions per year\n\nBaltimore City, Los Angeles County - Motor vehicle source") + labs(x = "Years", y = bquote("Changes in PM"[2.5] ~  "emissions per year (in tons)"))
+qplot(year, emissions, data = total.emissions.baltimore.LA.motor.vehicle, color = region, shape = region, geom = c("point", "line")) + ggtitle("Changes in emissions per year\n\nBaltimore City, Los Angeles County - Motor vehicle sources") + labs(x = "Years", y = bquote("Changes in PM"[2.5] ~  "emissions per year (in tons)"))
 dev.off()
